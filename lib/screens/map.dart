9847d4da88577c288e39fe8428fe659c82837ff8
username <okhremchuk12@gmail.com>
2023-12-20 16:19:13 +0200

    app

---
+++ lib/screens/map.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:places_app/models/place.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen(
+      {this.location = const PlaceLocation(
+        latitude: 37.422,
+        longitude: -122.084,
+        address: '',
+      ),
+      this.isSelecting = true,
+      super.key});
+
+  final PlaceLocation location;
+  final bool isSelecting;
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Text(widget.isSelecting ? 'Pick your location' : 'Your location'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: () {
+                Navigator.of(context).pop(_pickedLocation);
+              },
+            ),
+        ],
+      ),
+      body: GoogleMap(
+        onTap: !widget.isSelecting
+            ? null
+            : (position) {
+                setState(() {
+                  _pickedLocation = position;
+                });
+              },
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.location.latitude,
+            widget.location.longitude,
+          ),
+          zoom: 20,
+        ),
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('m1'),
+                  position: _pickedLocation ??
+                      LatLng(
+                        widget.location.latitude,
+                        widget.location.longitude,
+                      ),
+                ),
+              },
+      ),
+    );
+  }
+}
---
+++ lib/screens/places.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../providers/user_places.dart';
+import '../widgets/places_list.dart';
+import 'add_place.dart';
+
+class PlacesScreen extends ConsumerStatefulWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _PlacesScreenState();
+  }
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: FutureBuilder(
+          future: _placesFuture,
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(child: CircularProgressIndicator())
+                  : PlacesList(places: userPlaces),
+        ),
+      ),
+    );
+  }
+}
